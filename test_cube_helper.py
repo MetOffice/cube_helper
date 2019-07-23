1f0bea4a4660a38c07380e90808d7387b19c0dd5
TheElectricFlock <jbedwell@eld269.cmpd1.metoffice.gov.uk>
2019-07-23 14:33:08 +0100

    altered tests

--- test_cube_helper.py
+++ test_cube_helper.py
@@ -1,14 +1,15 @@
 import os
 import unittest
 import iris
-from ~/cube_helper import CubeHelper
+from cube_helper import CubeHelper
 
 class TestCubeHelper(unittest.TestCase):
 
     def test_initilisation(self):
-        test_dict = {'opt_constraint': None, 'opt_filetype': '.nc', 'loaded_cubes': [], 'cube_list': None,
-         'concatenated_cube': None, 'units': '', 'long_name': '', 'standard_name': '', 'is_unified': False,
-         'is_loaded': False, 'is_concatenated': False}
+        example_case = CubeHelper('data', opt_constraints='air_temperature')
+        self.assertEqual(example_case.directory, 'data')
+        self.assertEqual(example_case.opt_constraints, 'air_temperature')
+        self.assertEqual(type(example_case.cube_dataset.loaded_cubes), type([]))
 
-        example_case = CubeHelper()
-        self.assertEqual(example_case.__dict__, test_dict)
+if __name__ == "__main__":
+    unittest.main()
