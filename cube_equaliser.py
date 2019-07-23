978349d0a2591a6d7225e357c5f89dcf1166f830
TheElectricFlock <jbedwell@eld269.cmpd1.metoffice.gov.uk>
2019-07-23 13:05:51 +0100

    added static methods to cube_loader

--- cube_equaliser.py
+++ cube_equaliser.py
@@ -4,40 +4,39 @@ import numpy as np
 import cf_units
 
 
-class CubeEqualiser(object):
 
 
-	def equalise_attributes(self,cubes):
-		# Work out which attributes are identical across all the cubes.
-		common_keys = list(cubes[0].attributes.keys())
-		for cube in cubes[1:]:
-			cube_keys = list(cube.attributes.keys())
-			common_keys = [
-				key for key in common_keys
-				if (key in cube_keys and
-					np.all(cube.attributes[key] == cubes[0].attributes[key]))]
+def equalise_attributes(cubes):
+	# Work out which attributes are identical across all the cubes.
+	common_keys = list(cubes[0].attributes.keys())
+	for cube in cubes[1:]:
+		cube_keys = list(cube.attributes.keys())
+		common_keys = [
+			key for key in common_keys
+			if (key in cube_keys and
+				np.all(cube.attributes[key] == cubes[0].attributes[key]))]
 
-		# Remove all the other attributes.
-		for cube in cubes:
-			for key in list(cube.attributes.keys()):
-				if key not in common_keys:
-					del cube.attributes[key]
-
-
-	def unify_time_units(self, cubes):
-		epochs = {}
-		for cube in cubes:
-			for time_coord in cube.coords():
-				if time_coord.units.is_time_reference():
-					epoch = epochs.setdefault(time_coord.units.calendar, time_coord.units.origin)
-					new_unit = cf_units.Unit(epoch, time_coord.units.calendar)
-					time_coord.convert_units(new_unit)
+	# Remove all the other attributes.
+	for cube in cubes:
+		for key in list(cube.attributes.keys()):
+			if key not in common_keys:
+				del cube.attributes[key]
 
 
-	def remove_attributes(self, cubes):
-		attributes_list = list(cubes[0].attributes.keys())
-		for index, cube in enumerate(cubes):
-			for key in attributes_list:
-				cube.attributes[key] = ''
-				cubes[index] = cube
-		return iris.cube.CubeList(cubes)
+def unify_time_units(cubes):
+	epochs = {}
+	for cube in cubes:
+		for time_coord in cube.coords():
+			if time_coord.units.is_time_reference():
+				epoch = epochs.setdefault(time_coord.units.calendar, time_coord.units.origin)
+				new_unit = cf_units.Unit(epoch, time_coord.units.calendar)
+				time_coord.convert_units(new_unit)
+
+
+def remove_attributes(cubes):
+	attributes_list = list(cubes[0].attributes.keys())
+	for index, cube in enumerate(cubes):
+		for key in attributes_list:
+			cube.attributes[key] = ''
+			cubes[index] = cube
+	return iris.cube.CubeList(cubes)
--- cube_helper.py
+++ cube_helper.py
@@ -6,44 +6,38 @@ from cube_loader import CubeLoader
 
 class CubeHelper(object):
 
-	def __init__(self, directory, opt_filetype = ".nc", opt_constraints = None,):
-		self.directory = directory
-		if type(dir) == str:
-			loaded_cubes = CubeLoader.load_from_dir(self.directory, opt_filetype, opt_constraints)
-			self.cube_dataset = CubeSet(loaded_cubes)
-		elif type([]):
-			loaded_cubes = CubeLoader.load_from_filelist(self.directory, opt_filetype, opt_constraints)
-			self.cube_dataset = CubeSet(loaded_cubes)
-		else:
-			print("cube input parameters invalid")
+    def __init__(self, directory, opt_filetype = ".nc", opt_constraints = None,):
+        self.directory = directory
+        if type(directory) == str:
+            loaded_cubes = CubeLoader.load_from_dir(directory, opt_constraints, opt_filetype)
+            self.cube_dataset = CubeSet(loaded_cubes)
+        elif type(directory) == type([]):
+            loaded_cubes = CubeLoader.load_from_filelist(directory, opt_constraints, opt_filetype)
+            self.cube_dataset = CubeSet(loaded_cubes)
+        else:
+            print("cube input parameters invalid")
 
-	def get_combined_cube(self):
-		"""
-		Returns the concatenated_cube if cube_list has been concatenated.
+    def concatenate(self):
+        return self.cube_dataset.cube_list.concatenate()
 
-		:return iris.cube.Cube: concatenated resultant cube
-		"""
-		return None
+    def concatenate_cube(self):
+        return self.cube_dataset.cube_list.concatenate_cube()
 
+    def merge_cube(self):
+        return self.cube_dataset.cube_list.merge_cube()
 
-	def concatenate(self):
-		return self.cube_dataset.cube_list.concatenate()
+    def merge(self):
+        return self.cube_dataset.cube_list.merge()
 
-	def merge_cube(self):
-		return self.cube_dataset.cube_list.merge_cube()
+    def convert_units(self, unit):
+        for cube in self.cube_dataset.loaded_cubes:
+            cube.convert_units(unit)
 
-	def merge(self):
-		return self.cube_dataset.cube_list.merge()
-
-	def convert_units(self, unit):
-		for cube in self.cube_dataset.loaded_cubes:
-			cube.convert_units(unit)
-
-	# def collapse_dimension(self, dimension):
-	# 	for index, cube in enumerate(self.cube_dataset.loaded_cubes):
+    # def collapse_dimension(self, dimension):
+    # 	for index, cube in enumerate(self.cube_dataset.loaded_cubes):
     #
-	# 	self.cube_dataset.loaded_cubes[index] = cube.collapsed(dimension, iris.analysis.MEAN)
-	# 	self.cube_list = iris.cube.CubeList(self.cube_dataset.loaded_cubes)
+    # 	self.cube_dataset.loaded_cubes[index] = cube.collapsed(dimension, iris.analysis.MEAN)
+    # 	self.cube_list = iris.cube.CubeList(self.cube_dataset.loaded_cubes)
     #
-	# def remove_attributes(self, cubes):
-	# 	self.cube_list = CubeEqualiser.remove_attributes(cubes)
+    # def remove_attributes(self, cubes):
+    # 	self.cube_list = CubeEqualiser.remove_attributes(cubes)
