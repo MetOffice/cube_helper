75ee149ffa0ba0606416b40145754950e39171af
TheElectricFlock <jbedwell@eld269.cmpd1.metoffice.gov.uk>
2019-07-24 09:17:47 +0100

    changed file structure

---
+++ cube_helper/cube_dataset.py
@@ -0,0 +1,24 @@
+import os
+import iris
+from cube_equaliser import equalise_attributes, unify_time_units
+
+class CubeSet(object):
+
+	def __init__(self, loaded_cubes):
+		"""
+		initialises class. data_file list
+		is a list of cube filepaths or filenames to be manipulated.
+		"""
+		#self.loaded_cubes = loaded_cubes
+		self.cube_list = iris.cube.CubeList(loaded_cubes)
+		equalise_attributes(loaded_cubes)
+		unify_time_units(loaded_cubes)
+		self.loaded_cubes = loaded_cubes
+
+
+	def __repr__(self):
+		return '{self.cube_list}'.format(self=self)
+
+
+
+
