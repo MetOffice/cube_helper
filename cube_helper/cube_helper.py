67e8a4dea0513a30825df8c013a325ae27f47d8d
TheElectricFlock <jbedwell@eld269.cmpd1.metoffice.gov.uk>
2019-07-26 09:59:37 +0100

    added docstrings

--- cube_helper/cube_helper.py
+++ cube_helper/cube_helper.py
@@ -1,44 +1,79 @@
 import os
+import sys
 import iris
-from cube_helper.cube_helper.cube_loader import CubeLoader
-from cube_helper.cube_helper.cube_equaliser import remove_attributes, equalise_attributes, unify_time_units
+from cube_loader import CubeLoader
+from cube_equaliser import remove_attributes
+from cube_dataset import CubeSet
 
 
-class CubeHelper(object):
+class CubeHelp(object):
+    """
+    A wrapper class to implement methods provided by cube_equaliliser
+    """
+
 
     def __init__(self, directory, opt_filetype = ".nc", opt_constraints = None):
+        """
+        :param directory: The directory or list containing the set of cubes
+        :param opt_filetype: The filetype of the cubes (usually .nc or .pp)
+        :param opt_constraints: Any constraints to be applied when loading the cube
+        """
         self.directory = directory
         self.opt_filetype = opt_filetype
         self.opt_constraints = opt_constraints
         if type(directory) == str:
             loaded_cubes = CubeLoader.load_from_dir(directory, opt_constraints, opt_filetype)
-            self.cube_dataset = CubeSet(loaded_cubes)
+            if not loaded_cubes:
+                print("No cubes found")
+            else:
+                self.cube_dataset = CubeSet(loaded_cubes)
         elif type(directory) == type([]):
             loaded_cubes = CubeLoader.load_from_filelist(directory, opt_constraints, opt_filetype)
-            self.cube_dataset = CubeSet(loaded_cubes)
-        else:
-            print("cube input parameters invalid")
+            if not loaded_cubes:
+                print("No cubes found")
+            else:
+                self.cube_dataset = CubeSet(loaded_cubes)
 
     def concatenated(self):
+        """
+        :return self.cube_dataset.cube_list.concatenate(): The concatenated cubes of the cube_dataset
+        """
         return self.cube_dataset.cube_list.concatenate()
 
     def concatenated_cube(self):
+        """
+        :return self.cube_dataset.cube_list.concatenate_cube(): The concatenated cube of the cube_dataset:
+        """
         return self.cube_dataset.cube_list.concatenate_cube()
 
     def merged_cube(self):
+        """
+        :return self.cube_dataset.cube_list.merge_cube(): The merged cube of the cube_dataset
+        """
         return self.cube_dataset.cube_list.merge_cube()
 
     def merged(self):
+        """
+        :return self.cube_dataset.cube_list.merge(): The merged cubes of the cube_dataset
+        """
         return self.cube_dataset.cube_list.merge()
 
     def convert_units(self, unit):
+        """
+        :param unit: The approved unit to convert to.
+        """
         for cube in self.cube_dataset.loaded_cubes:
             cube.convert_units(unit)
 
     def collapsed_dimension(self, dimension):
+        """
+        :param dimension: the dimension of the cube to collapse
+        """
         for index, cube in enumerate(self.cube_dataset.loaded_cubes):
             self.cube_dataset.loaded_cubes[index] = cube.collapsed(dimension, iris.analysis.MEAN)
-            return iris.cube.CubeList(self.cube_dataset.loaded_cubes)
 
-    def remove_attributes(self, cubes):
-        remove_attributes(cubes)
+        self.cube_dataset.cube_list = iris.cube.CubeList(self.cube_dataset.loaded_cubes)
+
+
+    def remove_attributes(self):
+        remove_attributes(self.cube_dataset.loaded_cubes)
--- cube_helper/cube_loader.py
+++ cube_helper/cube_loader.py
@@ -10,9 +10,8 @@ class CubeLoader(object):
 		Loads a set of cubes from a given directory, single cubes are loaded and
 		appended into an iterable as well as being loaded into a cubelist.
 
-		:param string directory: directory to load data from
-		:return iris.cube.CubleList: CubeList loaded form directory
-		:return list loaded cubes: List of loaded cubes (needed to manipulate metadata etc)
+		:param string directory: Directory to load data from
+		:return list loaded cubes: List of cubes loaded from the filelist
 		"""
 		if opt_constraint == None:
 			loaded_cubes = []
@@ -35,9 +34,8 @@ class CubeLoader(object):
 		Loads a set of cubes from a given directory, single cubes are loaded and
 		appended into an iterable as well as being loaded into a cubelist.
 
-		:param list data_filelist: directory to load data from
-		:return iris.cube.CubleList: CubeList loaded from file list
-		:return list loaded_cubes: List of loaded cubes (needed to manipulate metadata etc)
+		:param list data_filelist: A list of files to load data from
+		:return list loaded_cubes: An iterable of cubes loaded from file list
 		"""
 		loaded_cubes = []
 		for filename in data_filelist:
