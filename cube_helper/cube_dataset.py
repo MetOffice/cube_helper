d75f7f08d053b3f60cd30156fc9217a3323b89df
TheElectricFlock <jbedwell@eld269.cmpd1.metoffice.gov.uk>
2019-07-29 11:23:11 +0100

    add test_convert_units

--- cube_helper/cube_dataset.py
+++ cube_helper/cube_dataset.py
@@ -1,6 +1,6 @@
 import os
 import iris
-from cube_helper.cube_helper.cube_equaliser import equalise_attributes, unify_time_units
+from cube_equaliser import equalise_attributes, unify_time_units
 
 class CubeSet(object):
 
@@ -9,7 +9,7 @@ class CubeSet(object):
 		initialises class. data_file list
 		is a list of cube filepaths or filenames to be manipulated.
 		"""
-		#self.loaded_cubes = loaded_cubes
+
 		self.cube_list = iris.cube.CubeList(loaded_cubes)
 		equalise_attributes(loaded_cubes)
 		unify_time_units(loaded_cubes)
