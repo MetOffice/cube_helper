730409e8b7abc70fd08fff7595d2b6c7397ffa08
TheElectricFlock <jbedwell@eld269.cmpd1.metoffice.gov.uk>
2019-07-30 13:32:01 +0100

    added test_remove_attributes method to TestCubeEqualiser

--- cube_helper/tests/test_cube_equaliser.py
+++ cube_helper/tests/test_cube_equaliser.py
@@ -7,8 +7,8 @@ from cube_equaliser import equalise_attributes,unify_time_units,unify_data_type,
 class TestCubeEqualiser(unittest.TestCase):
 
     def test_equalise_attributes(self):
-        filepath = '/project/champ/data/CMIP6/CMIP/MOHC/HadGEM3-GC31-LL/piControl/r1i1p1f1/Amon/tasmin/gn/v20190628'
-        test_load = CubeLoader.load_from_dir(filepath)
+        filepath = 'test_data/air_temp'
+        test_load = CubeLoader.load_from_dir(filepath, opt_filetype='.pp')
         equalise_attributes(test_load)
         for cubes in test_load:
             self.assertEqual(cubes.attributes, test_load[0].attributes)
@@ -21,6 +21,15 @@ class TestCubeEqualiser(unittest.TestCase):
             for time_coords in cube.coords():
                 if time_coords.units.is_time_reference():
                     self.assertEqual(cube[index].units.calendar, cube[index-1].units.calendar)
+
+    def test_remove_attributes(self):
+        filepath = 'test_data/air_temp'
+        test_load = CubeLoader.load_from_dir(filepath, opt_filetype='.pp')
+        remove_attributes(test_load)
+        keys = list(test_load[0].attributes.keys())
+        for cube in test_load:
+            for key in keys:
+                self.assertEqual(cube.attributes[key], '')
 
 
 if __name__ == "__main__":
