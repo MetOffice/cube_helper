f038272a21bd07400b99fe275afd5e8bc37f53e1
TheElectricFlock <jbedwell@vld046.cmpd1.metoffice.gov.uk>
2019-08-08 10:50:39 +0100

    tidying imports

--- cube_helper/__init__.py
+++ cube_helper/__init__.py
@@ -1,3 +1,1 @@
-from cube_helper.helper.cube_loader import CubeLoader
-from cube_helper.helper.cube_equaliser import remove_attributes, unify_data_type, unify_time_units, equalise_attributes
-from cube_helper.helper.cube_dataset import CubeSet
+from cube_helper.cube_help import CubeHelp
--- cube_helper/cube_help.py
+++ cube_helper/cube_help.py
@@ -1,7 +1,9 @@
 import os
 import sys
 import iris
-
+from cube_helper.cube_loader import CubeLoader
+from cube_helper.cube_dataset import CubeSet
+from cube_helper.cube_equaliser import remove_attributes, unify_time_units, equalise_attributes, unify_data_type
 
 class CubeHelp(object):
 	"""
