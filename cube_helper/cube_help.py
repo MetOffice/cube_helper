db528a1c5478f24e9f09e5956d538ae31854139f
TheElectricFlock <jbedwell@vld046.cmpd1.metoffice.gov.uk>
2019-08-08 16:47:43 +0100

    merge Imporved_docstrings branch

--- cube_helper/cube_help.py
+++ cube_helper/cube_help.py
@@ -1,7 +1,7 @@
 import iris
 from cube_helper.cube_loader import CubeLoader
 from cube_helper.cube_dataset import CubeSet
-from cube_helper.cube_equaliser import remove_attributes, unify_data_type, unify_time_units, equalise_attributes
+from cube_helper.cube_equaliser import remove_attributes, unify_time_units, equalise_attributes, unify_data_type
 
 
 class CubeHelp(object):
