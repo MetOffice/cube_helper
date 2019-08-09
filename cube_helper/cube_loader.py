22a1972d0884f3c106013fa0ae7d66ff19a13a04
TheElectricFlock <jbedwell@vld046.cmpd1.metoffice.gov.uk>
2019-08-09 11:26:14 +0100

    added google docstrings to cube_loader

--- cube_helper/cube_loader.py
+++ cube_helper/cube_loader.py
@@ -4,56 +4,79 @@ import iris
 
 class CubeLoader(object):
 
+    @staticmethod
+    def load_from_dir(directory, opt_constraint=None, opt_filetype='.nc'):
+        """
+        Loads a set of cubes from a given directory, single cubes are loaded
+        And appended into an iterable then returned as a CubeList.
 
-	@staticmethod
-	def load_from_dir(directory, opt_constraint=None, opt_filetype='.nc'):
-		"""
-		Loads a set of cubes from a given directory, single cubes are loaded and
-		appended into an iterable then returned as a CubeList.
+        Args:
+            directory: a chosen directory
+            to operate on.
 
-		:param string directory: Directory to load data from
-		:return iris.CubeList: List of cubes loaded from the directory
-		"""
-		if opt_constraint == None:
-			loaded_cubes = []
-			for path in os.listdir(directory):
-				full_path = os.path.join(directory, path)
-				if os.path.isfile(full_path) and full_path.endswith(opt_filetype):
-					loaded_cubes.append(iris.load_cube(full_path))
-			return iris.cube.CubeList(loaded_cubes)
-		else:
-			loaded_cubes = []
-			for path in os.listdir(directory):
-				full_path = os.path.join(directory, path)
-				if os.path.isfile(full_path) and full_path.endswith(opt_filetype):
-					loaded_cubes.append(iris.load_cube(full_path, opt_constraint))
-			return iris.cube.CubeList(loaded_cubes)
+            opt_filetype (optional): a string specifying the expected type
+            Of files found in the dataset
 
-	@staticmethod
-	def load_from_filelist(data_filelist, opt_constraint=None, opt_filetype='.nc'):
-		"""
-		Loads a set of cubes from a given directory, single cubes are loaded and
-		appended into an iterable as well as being loaded into a cubelist.
+            opt_constraints (optional): a string specifying any constraints
+            You wish to load the dataset with.
 
-		:param list data_filelist: A list of files to load data from
-		:return iris.Cubelist: List of cubes loaded from the list of filenames
-		"""
-		loaded_cubes = []
-		for filename in data_filelist:
-			if filename.endswith(opt_filetype):
-				break
-			else:
-				print('\n\nThe selected filetype is not present in data_filelist\n\n')
+        Returns:
+            iris.cube.CubeList(loaded_cubes), a CubeList of the loaded
+            Cubes.
+        """
+        if opt_constraint is None:
+            loaded_cubes = []
+            for path in os.listdir(directory):
+                full_path = os.path.join(directory, path)
+                if os.path.isfile(full_path):
+                    if full_path.endswith(opt_filetype):
+                        loaded_cubes.append(iris.load_cube(full_path))
+            return iris.cube.CubeList(loaded_cubes)
+        else:
+            loaded_cubes = []
+            for path in os.listdir(directory):
+                full_path = os.path.join(directory, path)
+                if os.path.isfile(full_path):
+                    if full_path.endswith(opt_filetype):
+                        loaded_cubes.append(iris.load_cube(full_path,
+                                                           opt_constraint))
+            return iris.cube.CubeList(loaded_cubes)
 
-		for filename in data_filelist:
-			if opt_constraint == None:
-				try:
-					loaded_cubes.append(iris.load_cube(filename))
-				except:
-					pass
-			else:
-				try:
-					loaded_cubes.append(iris.load_cube(filename, opt_constraint))
-				except:
-					pass
-		return iris.cube.CubeList(loaded_cubes)
+    @staticmethod
+    def load_from_filelist(data_filelist, opt_constraint=None,
+                           opt_filetype='.nc'):
+        """
+        Loads a set of cubes from a given directory, single cubes are
+        Loaded and appended into an iterable as well as being loaded into a
+        CubeList.
+
+        Args:
+            data_filelist: a chosen list of filenames to operate on.
+
+            opt_filetype (optional): a string specifying the expected type
+            Of files found in the dataset
+
+            opt_constraints (optional): a string specifying any constraints
+            You wish to load the dataset with.
+
+        Returns:
+            iris.cube.CubeList(loaded_cubes), a CubeList of the loaded
+            Cubes.
+        """
+        loaded_cubes = []
+        for filename in data_filelist:
+            if filename.endswith(opt_filetype):
+                break
+            else:
+                print('\n\nThe selected filetype is '
+                      'not present in data_filelist\n\n')
+
+        for filename in data_filelist:
+            if opt_constraint is None:
+                loaded_cubes.append(iris.load_cube(filename))
+
+            else:
+
+                loaded_cubes.append(iris.load_cube(filename, opt_constraint))
+
+        return iris.cube.CubeList(loaded_cubes)
