4e9a5c6431eb79c1bb14e399e6601dc9409da1ee
James Bedwell <44397706+TheElectricFlock@users.noreply.github.com>
2019-08-16 14:30:43 +0100

    Change tests to make use of AssertIsInstance()

--- tests/test_cube_help.py
+++ tests/test_cube_help.py
@@ -14,36 +14,36 @@ class TestCubeHelper(unittest.TestCase):
 		example_case = CubeHelp('test_data/air_temp', opt_constraints='air_temperature', opt_filetype='.pp')
 		self.assertEqual(example_case.directory, 'test_data/air_temp')
 		self.assertEqual(example_case.opt_constraints, 'air_temperature')
-		self.assertEqual(type(example_case.cube_dataset.cube_list), iris.cube.CubeList)
+		self.assertIsInstance(example_case.cube_dataset.cube_list, iris.cube.CubeList)
 		filelist = ['test_data/air_temp/air_temp_1.pp','test_data/air_temp/air_temp_2.pp',
 					'test_data/air_temp/air_temp_3.pp','test_data/air_temp/air_temp_4.pp'
 											'test_data/air_temp/air_temp_5.pp']
 		example_case = CubeHelp(filelist, opt_filetype='.pp')
-		self.assertEqual(type(example_case.directory), list)
+		self.assertIsInstance(example_case.directory, list)
 
 	def test_concatenated_cube(self):
 		filepath = '/project/champ/data/CMIP6/CMIP/MOHC/HadGEM3-GC31-LL/piControl/r1i1p1f1/Amon/tasmin/gn/v20190628'
 		example_case = CubeHelp(filepath)
-		self.assertEqual(type(example_case.cube_dataset.cube_list), iris.cube.CubeList)
+		self.assertIsInstance(example_case.cube_dataset.cube_list, iris.cube.CubeList)
 		example_case.equalise()
 		test_method = example_case.concatenated_cube()
-		self.assertEqual(type(test_method), iris.cube.Cube)
+		self.assertIsInstance(test_method, iris.cube.Cube)
 		self.assertEqual(test_method.ndim, 3)
 
 	def test_concatenated(self):
 		example_case = CubeHelp('test_data/north_sea_ice', opt_filetype='.pp')
 		test_method = example_case.concatenated()
-		self.assertEqual(type(test_method), iris.cube.CubeList)
+		self.assertIsInstance(test_method, iris.cube.CubeList)
 
 	def test_merged(self):
 		example_case = CubeHelp('test_data/north_sea_ice', opt_filetype='.pp')
 		test_method = example_case.merged()
-		self.assertEqual(type(test_method), iris.cube.CubeList)
+		self.assertIsInstance(test_method, iris.cube.CubeList)
 
 	def test_merged_cube(self):
 		example_case = CubeHelp('test_data/north_sea_ice', opt_filetype='.pp')
 		test_method = example_case.merged_cube()
-		self.assertEqual(type(test_method), iris.cube.Cube)
+		self.assertIsInstance(test_method, iris.cube.Cube)
 		self.assertEqual(test_method.ndim, 3)
 
 
