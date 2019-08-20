a7b64c2e9558ca025672e6b8fbb460316350a3bd
TheElectricFlock <jbedwell@vld046.cmpd1.metoffice.gov.uk>
2019-08-20 09:38:02 +0100

    removed CubeLoader class in favor for module

--- tests/test_cube_loader.py
+++ tests/test_cube_loader.py
@@ -1,6 +1,6 @@
 import unittest
 import iris
-from cube_loader import CubeLoader
+from cube_helper.cube_loader import load_from_dir, load_from_filelist
 
 
 
@@ -11,11 +11,11 @@ class TestCubeLoader(unittest.TestCase):
         filelist = ['test_data/air_temp/air_temp_1.pp', 'test_data/air_temp/air_temp_2.pp',
                     'test_data/air_temp/air_temp_3.pp', 'test_data/air_temp/air_temp_4.pp',
                                                         'test_data/air_temp/air_temp_5.pp']
-        example_case = CubeLoader.load_from_filelist(filelist, filetype='.pp')
+        example_case = load_from_filelist(filelist, filetype='.pp')
         self.assertEqual(type(example_case), iris.cube.CubeList)
 
     def test_load_from_dir(self):
-        example_case = CubeLoader.load_from_dir('test_data/air_temp', filetype='.pp')
+        example_case = load_from_dir('test_data/air_temp', filetype='.pp')
         self.assertEqual(type(example_case), iris.cube.CubeList)
 
 
