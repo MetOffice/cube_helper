0c1e573748857a447d3ef1e452c47f11a2f552d1
TheElectricFlock <jbedwell@vld046.cmpd1.metoffice.gov.uk>
2019-09-11 11:26:03 +0100

    alter tests for test_cube_loader

--- tests/test_cube_loader.py
+++ tests/test_cube_loader.py
@@ -8,11 +8,11 @@ class TestCubeLoader(unittest.TestCase):
                     'test_data/air_temp/air_temp_3.pp', 'test_data/air_temp/air_temp_4.pp',
                                                         'test_data/air_temp/air_temp_5.pp']
         example_case = load_from_filelist(filelist, '.pp')
-        self.assertEqual(type(example_case), iris.cube.CubeList)
+        self.assertIsInstance(example_case, list)
 
     def test_load_from_dir(self):
         example_case = load_from_dir('test_data/air_temp/', '.pp')
-        self.assertEqual(type(example_case), iris.cube.CubeList)
+        self.assertIsInstance(example_case, list)
 
     def test_parse_directory(self):
         directory = 'test_data/air_temp/air_temp_1.pp'
