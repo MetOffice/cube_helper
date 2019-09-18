0c3c1cabd7740c4ecd5e231785ec13ab7a25e64d
TheElectricFlock <jbedwell@vld046.cmpd1.metoffice.gov.uk>
2019-09-18 16:07:01 +0100

    Add test_initialisation

--- tests/test_cube_dataset.py
+++ tests/test_cube_dataset.py
@@ -1,4 +1,5 @@
 import unittest
+import iris
 from cube_helper.cube_loader import load_from_dir
 from cube_helper.cube_dataset import CubeSet
 
@@ -6,4 +7,12 @@ from cube_helper.cube_dataset import CubeSet
 class TestCubeDataset(unittest.TestCase):
 
     def test_initialisation(self):
-        
+        filepath = 'test_data/north_sea_ice'
+        cube_list = load_from_dir(filepath, '.pp')
+        cube_set = CubeSet(cube_list)
+        self.assertIsInstance(cube_list, list)
+        self.assertIsInstance(cube_set, iris.cube.CubeList)
+                
+if __name__ == "__main__":
+    unittest.main()
+
