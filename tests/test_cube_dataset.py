4f9cb2c891ca11b3509dd9d6339e5dca864f1f16
TheElectricFlock <jbedwell@vld046.cmpd1.metoffice.gov.uk>
2019-09-24 15:49:56 +0100

    Modify to conform to pep8

--- tests/test_cube_dataset.py
+++ tests/test_cube_dataset.py
@@ -5,14 +5,14 @@ from cube_helper.cube_dataset import CubeSet
 
 
 class TestCubeDataset(unittest.TestCase):
-
     def test_initialisation(self):
         filepath = 'test_data/north_sea_ice'
         cube_list = load_from_dir(filepath, '.pp')
         cube_set = CubeSet(cube_list)
         self.assertIsInstance(cube_list, list)
         self.assertIsInstance(cube_set, iris.cube.CubeList)
-        self.assertEqual(dir(cube_set),dir(iris.cube.CubeList) )
+        self.assertEqual(dir(cube_set), dir(iris.cube.CubeList))
+
+
 if __name__ == "__main__":
     unittest.main()
-
