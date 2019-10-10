50f61cbb84af178af7d4821248ff0d06f92903b1
James Bedwell <https://theelectricflock@github.com>
2019-10-10 11:26:09 +0100

    Refractor tests

--- tests/test_cube_equaliser.py
+++ tests/test_cube_equaliser.py
@@ -1,27 +1,29 @@
 import unittest
 import iris
-from cube_helper.cube_loader import load_from_dir
+import glob
 from cube_helper.cube_equaliser import (_sort_by_earliest_date,
                                         equalise_attributes,
                                         equalise_time_units,
                                         equalise_aux_coords,
                                         equalise_dim_coords,
-                                        equalise_data_type)
+                                        equalise_data_type,
+                                        remove_attributes)
 
 
 class TestCubeEqualiser(unittest.TestCase):
 
     def test_equalise_attributes(self):
-        filepath = 'test_data/air_temp'
-        test_load = load_from_dir(filepath, filetype='.pp')
-        equalise_attributes(test_load)
+        filepaths = glob.glob('test_data/air_temp/*.pp')
+        test_load = [iris.load_cube(cube) for cube in filepaths]
+        test_load = equalise_attributes(test_load)
         for cubes in test_load:
-            self.assertEqual(cubes.attributes, test_load[0].attributes)
+            self.assertEqual(cubes.attributes,
+                             test_load[0].attributes)
 
-    def test_unify_time_units(self):
-        filepath = 'test_data/air_temp'
-        test_load = load_from_dir(filepath, filetype='.pp')
-        equalise_time_units(test_load)
+    def test_equalise_time_units(self):
+        filepaths = glob.glob('test_data/air_temp/*.pp')
+        test_load = [iris.load_cube(cube) for cube in filepaths]
+        test_load = equalise_time_units(test_load)
         for index, cube in enumerate(test_load):
             for time_coords in cube.coords():
                 if time_coords.units.is_time_reference():
@@ -29,8 +31,8 @@ class TestCubeEqualiser(unittest.TestCase):
                                      cube[index-1].units.calendar)
 
     def test_remove_attributes(self):
-        filepath = 'test_data/air_temp'
-        test_load = load_from_dir(filepath, filetype='.pp')
+        filepaths = glob.glob('test_data/air_temp/*.pp')
+        test_load = [iris.load_cube(cube) for cube in filepaths]
         remove_attributes(test_load)
         keys = list(test_load[0].attributes.keys())
         for cube in test_load:
@@ -38,8 +40,8 @@ class TestCubeEqualiser(unittest.TestCase):
                 self.assertEqual(cube.attributes[key], '')
 
     def test_equalise_data_type(self):
-        filepath = 'test_data/air_temp'
-        test_load = load_from_dir(filepath, filetype='.pp')
+        filepaths = glob.glob('test_data/air_temp/*.pp')
+        test_load = [iris.load_cube(cube) for cube in filepaths]
         equalise_data_type(test_load)
         for cube in test_load:
             self.assertEqual(cube.dtype, 'float32')
@@ -54,50 +56,79 @@ class TestCubeEqualiser(unittest.TestCase):
             self.assertEqual(cube.dtype, 'int64')
 
     def test_equalise_dim_coords(self):
-        filepath = 'test_data/air_temp'
-        test_load = load_from_dir(filepath, filetype='.pp')
-        equalise_dim_coords(test_load)
+        filepaths = glob.glob('test_data/air_temp/*.pp')
+        test_load = [iris.load_cube(cube) for cube in filepaths]
+        test_load = equalise_dim_coords(test_load)
         for cube in test_load:
             self.assertEqual(cube.dim_coords[0].name(), 'latitude')
             self.assertEqual(cube.dim_coords[1].name(), 'longitude')
 
     def test_equalise_aux_coords(self):
-        filepath = '/project/champ/data/cmip5/output1/ICHEC/EC-EARTH/' \
-                   'rcp85/mon/atmos/Amon/r1i1p1/v20171115/tas'
-        test_load = load_from_dir(filepath, filetype='.nc')
-        equalise_aux_coords(test_load)
+        filepaths = glob.glob('/project/champ/data/cmip5/output1/ICHEC/EC-EARTH/'
+                             'rcp85/mon/atmos/Amon/r1i1p1/v20171115/tas/*.nc')
+        test_load = [iris.load_cube(cube) for cube in filepaths]
+        test_load = equalise_aux_coords(test_load)
         for cube in test_load:
             coords_list = [c.name() for c in cube.coords()]
             self.assertNotIn('height', coords_list)
 
     def test_sort_by_earliest_date(self):
-        filepath = '/project/champ/data/cmip5/output1/ICHEC/EC-EARTH/' \
-                   'rcp85/mon/atmos/Amon/r1i1p1/v20171115/tas'
-        test_load = load_from_dir(filepath, filetype='.nc')
-        cube_list = iris.cube.CubeList(test_load)
-        cube_list.sort(key=_sort_by_earliest_date)
-        self.assertEqual(cube_list[0].dim_coords[0].units.origin,
+        filepaths = glob.glob('/project/champ/data/cmip5/output1/ICHEC/EC-EARTH/'
+                             'rcp85/mon/atmos/Amon/r1i1p1/v20171115/tas/*.nc')
+        test_load = [iris.load_cube(cube) for cube in filepaths]
+        test_load = iris.cube.CubeList(test_load)
+        test_load.sort(key=_sort_by_earliest_date)
+        self.assertEqual(test_load[0].dim_coords[0].units.origin,
                          'days since 2006-01-01 00:00:00')
-        self.assertEqual(cube_list[1].dim_coords[0].units.origin,
+        self.assertEqual(test_load[1].dim_coords[0].units.origin,
                          'days since 2010-01-01 00:00:00')
-        self.assertEqual(cube_list[2].dim_coords[0].units.origin,
+        self.assertEqual(test_load[2].dim_coords[0].units.origin,
                          'days since 2020-01-01 00:00:00')
-        self.assertEqual(cube_list[3].dim_coords[0].units.origin,
+        self.assertEqual(test_load[3].dim_coords[0].units.origin,
                          'days since 2030-01-01 00:00:00')
-        self.assertEqual(cube_list[4].dim_coords[0].units.origin,
+        self.assertEqual(test_load[4].dim_coords[0].units.origin,
                          'days since 2040-01-01 00:00:00')
-        self.assertEqual(cube_list[5].dim_coords[0].units.origin,
+        self.assertEqual(test_load[5].dim_coords[0].units.origin,
                          'days since 2050-01-01 00:00:00')
-        self.assertEqual(cube_list[6].dim_coords[0].units.origin,
+        self.assertEqual(test_load[6].dim_coords[0].units.origin,
                          'days since 2060-01-01 00:00:00')
-        self.assertEqual(cube_list[7].dim_coords[0].units.origin,
+        self.assertEqual(test_load[7].dim_coords[0].units.origin,
                          'days since 2070-01-01 00:00:00')
-        self.assertEqual(cube_list[8].dim_coords[0].units.origin,
+        self.assertEqual(test_load[8].dim_coords[0].units.origin,
                          'days since 2080-01-01 00:00:00')
-        self.assertEqual(cube_list[9].dim_coords[0].units.origin,
+        self.assertEqual(test_load[9].dim_coords[0].units.origin,
                          'days since 2090-01-01 00:00:00')
-        self.assertEqual(cube_list[10].dim_coords[0].units.origin,
+        self.assertEqual(test_load[10].dim_coords[0].units.origin,
                          'days since 2100-1-1')
+
+    def test_file_sort_by_earliest_date(self):
+        filepaths = glob.glob('/project/champ/data/cmip5/output1/ICHEC/EC-EARTH/'
+                             'rcp85/mon/atmos/Amon/r1i1p1/v20171115/tas/*.nc')
+        test_load = [iris.load_cube(cube) for cube in filepaths]
+        test_load.sort(key=_sort_by_earliest_date)
+        self.assertEqual(test_load[0].dim_coords[0].units.origin,
+                         'days since 2006-01-01 00:00:00')
+        self.assertEqual(test_load[1].dim_coords[0].units.origin,
+                         'days since 2010-01-01 00:00:00')
+        self.assertEqual(test_load[2].dim_coords[0].units.origin,
+                         'days since 2020-01-01 00:00:00')
+        self.assertEqual(test_load[3].dim_coords[0].units.origin,
+                         'days since 2030-01-01 00:00:00')
+        self.assertEqual(test_load[4].dim_coords[0].units.origin,
+                         'days since 2040-01-01 00:00:00')
+        self.assertEqual(test_load[5].dim_coords[0].units.origin,
+                         'days since 2050-01-01 00:00:00')
+        self.assertEqual(test_load[6].dim_coords[0].units.origin,
+                         'days since 2060-01-01 00:00:00')
+        self.assertEqual(test_load[7].dim_coords[0].units.origin,
+                         'days since 2070-01-01 00:00:00')
+        self.assertEqual(test_load[8].dim_coords[0].units.origin,
+                         'days since 2080-01-01 00:00:00')
+        self.assertEqual(test_load[9].dim_coords[0].units.origin,
+                         'days since 2090-01-01 00:00:00')
+        self.assertEqual(test_load[10].dim_coords[0].units.origin,
+                         'days since 2100-1-1')
+
 
 
 if __name__ == "__main__":
