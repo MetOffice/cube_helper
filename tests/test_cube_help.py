1e98c1a35b2959c61ed48fa75401a586127e0d89
James Bedwell <https://theelectricflock@github.com>
2019-10-21 11:05:08 +0100

    conform to Pep8

--- tests/test_cube_help.py
+++ tests/test_cube_help.py
@@ -4,8 +4,8 @@ from glob import glob
 
 
 def tes_concatenate():
-    filepaths = glob('/project/champ/data/cmip5/output1/ICHEC/EC-EARTH/'
-                         'rcp85/mon/atmos/Amon/r1i1p1/v20171115/tas/*.nc')
+    filepaths = glob('/project/champ/data/cmip5/output1/ICHEC/EC-EARTH'
+                     '/rcp85/mon/atmos/Amon/r1i1p1/v20171115/tas/*.nc')
     test_load = [iris.load_cube(cube) for cube in filepaths]
     test_case_a = concatenate(test_load)
     test_load = iris.cube.CubeList(test_load)
@@ -13,23 +13,29 @@ def tes_concatenate():
     assert isinstance(test_case_a, iris.cube.Cube)
     assert isinstance(test_case_b, iris.cube.Cube)
 
+
 def test_cube_load():
     filepaths = glob('/project/champ/data/cmip5/output1/ICHEC/EC-EARTH/'
-                         'rcp85/mon/atmos/Amon/r1i1p1/v20171115/tas/*.nc')
+                     'rcp85/mon/atmos/Amon/r1i1p1/v20171115/tas/*.nc')
     directory = '/project/champ/data/cmip5/output1/ICHEC/EC-EARTH/' \
                 'rcp85/mon/atmos/Amon/r1i1p1/v20171115/tas/'
 
     test_case_a = cube_load(filepaths)
     assert isinstance(test_case_a, iris.cube.Cube)
-    assert test_case_a.dim_coords[0].units.origin == "days since 2006-01-01 00:00:00"
+    assert test_case_a.dim_coords[0].units.origin == "days" \
+                                                     " since 2006-01-01" \
+                                                     " 00:00:00"
     assert test_case_a.dim_coords[0].units.calendar == "gregorian"
     test_case_b = cube_load(directory)
-    assert test_case_b.dim_coords[0].units.origin == "days since 2006-01-01 00:00:00"
+    assert test_case_b.dim_coords[0].units.origin == "days" \
+                                                     " since 2006-01-01" \
+                                                     " 00:00:00"
     assert test_case_b.dim_coords[0].units.calendar == "gregorian"
+
 
 def test_add_catergorical():
     filepaths = glob('/project/champ/data/cmip5/output1/ICHEC/EC-EARTH/'
-                         'rcp85/mon/atmos/Amon/r1i1p1/v20171115/tas/*.nc')
+                     'rcp85/mon/atmos/Amon/r1i1p1/v20171115/tas/*.nc')
     test_case_a = cube_load(filepaths)
     test_case_b = [iris.load_cube(cube) for cube in filepaths]
     test_catergoricals = ["season_year", "season_number",
@@ -49,4 +55,3 @@ def test_add_catergorical():
             cube = add_categorical(catergorical, cube)
             assert cube.coord(catergorical)
             cube.remove_coord(catergorical)
-
