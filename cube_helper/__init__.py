79d3950dc3b8705261e6e1792be220de306e1d40
James Bedwell <https://theelectricflock@github.com>
2019-10-30 10:10:04 +0000

    removed private functions from __init__.py

--- cube_helper/__init__.py
+++ cube_helper/__init__.py
@@ -1,10 +1,8 @@
-from cube_helper.cube_help import (cube_load, add_categorical,
-                                   _add_categorical, concatenate)
+from cube_helper.cube_help import (cube_load, add_categorical, concatenate)
 from cube_helper.cube_loader import (load_from_dir,
                                      load_from_filelist,
                                      sort_by_earliest_date,
-                                     file_sort_by_earliest_date,
-                                     _sort_by_date)
+                                     file_sort_by_earliest_date)
 from cube_helper.cube_equaliser import (examine_dim_bounds,
                                         equalise_time_units,
                                         equalise_attributes,
