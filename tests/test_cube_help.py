73d095c345249cac7f496bbed151f49506aec0d3
James Bedwell <https://theelectricflock@github.com>
2019-10-30 10:27:52 +0000

    Fixed spelling errors

--- tests/test_cube_help.py
+++ tests/test_cube_help.py
@@ -3,7 +3,7 @@ from cube_helper.cube_help import cube_load, concatenate, add_categorical
 from glob import glob
 
 
-def tes_concatenate():
+def test_concatenate():
     filepaths = glob('/project/champ/data/cmip5/output1/ICHEC/EC-EARTH'
                      '/rcp85/mon/atmos/Amon/r1i1p1/v20171115/tas/*.nc')
     test_load = [iris.load_cube(cube) for cube in filepaths]
@@ -33,25 +33,25 @@ def test_cube_load():
     assert test_case_b.dim_coords[0].units.calendar == "gregorian"
 
 
-def test_add_catergorical():
+def test_add_categorical():
     filepaths = glob('/project/champ/data/cmip5/output1/ICHEC/EC-EARTH/'
                      'rcp85/mon/atmos/Amon/r1i1p1/v20171115/tas/*.nc')
     test_case_a = cube_load(filepaths)
     test_case_b = [iris.load_cube(cube) for cube in filepaths]
-    test_catergoricals = ["season_year", "season_number",
+    test_categoricals = ["season_year", "season_number",
                           "season_membership", "season",
                           "year", "month_number",
                           "month_fullname", "month",
                           "day_of_month", "day_of_year",
                           "weekday_number", "weekday_fullname",
                           "weekday", "hour"]
-    for catergorical in test_catergoricals:
-        test_case_a = add_categorical(catergorical, test_case_a)
-        assert test_case_a.coord(catergorical)
-        test_case_a.remove_coord(catergorical)
+    for categorical in test_categoricals:
+        test_case_a = add_categorical(categorical, test_case_a)
+        assert test_case_a.coord(categorical)
+        test_case_a.remove_coord(categorical)
 
-    for catergorical in test_catergoricals:
+    for categorical in test_categoricals:
         for cube in test_case_b:
-            cube = add_categorical(catergorical, cube)
-            assert cube.coord(catergorical)
-            cube.remove_coord(catergorical)
+            cube = add_categorical(categorical, cube)
+            assert cube.coord(categorical)
+            cube.remove_coord(categorical)
