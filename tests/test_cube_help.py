12a232be70988e871807732411dc2cfb7070d9bf
James Bedwell <https://theelectricflock@github.com>
2019-11-04 13:33:32 +0000

    Made test_cube_help.py more portable

--- tests/test_cube_help.py
+++ tests/test_cube_help.py
@@ -1,11 +1,12 @@
 import iris
 from cube_helper.cube_help import cube_load, concatenate, add_categorical
 from glob import glob
-
+import os
 
 def test_concatenate():
-    filepaths = glob('/project/champ/data/cmip5/output1/ICHEC/EC-EARTH'
-                     '/rcp85/mon/atmos/Amon/r1i1p1/v20171115/tas/*.nc')
+    abs_path = os.path.dirname(os.path.abspath(__file__))
+    glob_path = abs_path + '/test_data/realistic_3d_time' + '/*.nc'
+    filepaths = glob(glob_path)
     test_load = [iris.load_cube(cube) for cube in filepaths]
     test_case_a = concatenate(test_load)
     test_load = iris.cube.CubeList(test_load)
@@ -15,27 +16,27 @@ def test_concatenate():
 
 
 def test_cube_load():
-    filepaths = glob('/project/champ/data/cmip5/output1/ICHEC/EC-EARTH/'
-                     'rcp85/mon/atmos/Amon/r1i1p1/v20171115/tas/*.nc')
-    directory = '/project/champ/data/cmip5/output1/ICHEC/EC-EARTH/' \
-                'rcp85/mon/atmos/Amon/r1i1p1/v20171115/tas/'
-
+    abs_path = os.path.dirname(os.path.abspath(__file__))
+    glob_path = abs_path + '/test_data/realistic_3d_time' + '/*.nc'
+    filepaths = glob(glob_path)
+    directory = abs_path + '/test_data/realistic_3d_time'
     test_case_a = cube_load(filepaths)
     assert isinstance(test_case_a, iris.cube.Cube)
-    assert test_case_a.dim_coords[0].units.origin == "days" \
-                                                     " since 2006-01-01" \
+    assert test_case_a.dim_coords[0].units.origin == "hours" \
+                                                     " since 1970-01-01" \
                                                      " 00:00:00"
     assert test_case_a.dim_coords[0].units.calendar == "gregorian"
     test_case_b = cube_load(directory)
-    assert test_case_b.dim_coords[0].units.origin == "days" \
-                                                     " since 2006-01-01" \
+    assert test_case_b.dim_coords[0].units.origin == "hours" \
+                                                     " since 1970-01-01" \
                                                      " 00:00:00"
     assert test_case_b.dim_coords[0].units.calendar == "gregorian"
 
 
 def test_add_categorical():
-    filepaths = glob('/project/champ/data/cmip5/output1/ICHEC/EC-EARTH/'
-                     'rcp85/mon/atmos/Amon/r1i1p1/v20171115/tas/*.nc')
+    abs_path = os.path.dirname(os.path.abspath(__file__))
+    glob_path = abs_path + '/test_data/realistic_3d_time' + '/*.nc'
+    filepaths = glob(glob_path)
     test_case_a = cube_load(filepaths)
     test_case_b = [iris.load_cube(cube) for cube in filepaths]
     test_categoricals = ["season_year", "season_number",
