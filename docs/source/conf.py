50229acf5e3b13645f68d80abf33b389b8fcdc12
James Bedwell <https://theelectricflock@github.com>
2019-11-05 10:22:57 +0000

    Remove documentation build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -97,7 +97,7 @@ html_static_path = ['_static']
 # default: ``['localtoc.html', 'relations.html', 'sourcelink.html',
 # 'searchbox.html']``.
 #
-# html_sidebars = {}
+html_sidebars = {'cube_helper.html'}
 
 
 # -- Options for HTMLHelp output ---------------------------------------------
