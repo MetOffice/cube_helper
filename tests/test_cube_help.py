5515a7b574033beeab0e253adb31aa4bc37acb8f
James Bedwell <https://theelectricflock@github.com>
2019-11-11 12:20:28 +0000

    Changed name of cube_load() to load()

--- tests/test_cube_help.py
+++ tests/test_cube_help.py
@@ -1,5 +1,5 @@
 import iris
-from cube_helper.cube_help import cube_load, concatenate, add_categorical
+from cube_helper.cube_help import load, concatenate, add_categorical
 from glob import glob
 import os
 
@@ -15,18 +15,18 @@ def test_concatenate():
     assert isinstance(test_case_b, iris.cube.Cube)
 
 
-def test_cube_load():
+def test_load():
     abs_path = os.path.dirname(os.path.abspath(__file__))
     glob_path = abs_path + '/test_data/realistic_3d_time' + '/*.nc'
     filepaths = glob(glob_path)
     directory = abs_path + '/test_data/realistic_3d_time'
-    test_case_a = cube_load(filepaths)
+    test_case_a = load(filepaths)
     assert isinstance(test_case_a, iris.cube.Cube)
     assert test_case_a.dim_coords[0].units.origin == "hours" \
                                                      " since 1970-01-01" \
                                                      " 00:00:00"
     assert test_case_a.dim_coords[0].units.calendar == "gregorian"
-    test_case_b = cube_load(directory)
+    test_case_b = load(directory)
     assert test_case_b.dim_coords[0].units.origin == "hours" \
                                                      " since 1970-01-01" \
                                                      " 00:00:00"
@@ -37,7 +37,7 @@ def test_add_categorical():
     abs_path = os.path.dirname(os.path.abspath(__file__))
     glob_path = abs_path + '/test_data/realistic_3d_time' + '/*.nc'
     filepaths = glob(glob_path)
-    test_case_a = cube_load(filepaths)
+    test_case_a = load(filepaths)
     test_case_b = [iris.load_cube(cube) for cube in filepaths]
     test_categoricals = ["season_year", "season_number",
                           "season_membership", "season",
