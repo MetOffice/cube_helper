fbd209ac12d2daae184f3407e8830aff2865ba32
James Bedwell <https://theelectricflock@github.com>
2020-03-13 15:17:32 +0000

    PEP8 conform

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -41,5 +41,6 @@ class TestLogger(unittest.TestCase):
                           'and history attributes'
         self.assertEqual(output, expected_output)
 
+
 if __name__ == '__main__':
     unittest.main()
