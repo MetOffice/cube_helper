46efa52d61e5f6bf74c4195eee808661bcd234b5
James Bedwell <https://theelectricflock@github.com>
2020-06-19 15:54:24 +0100

    Add module level access to fucntions

--- cube_helper/__init__.py
+++ cube_helper/__init__.py
@@ -8,7 +8,9 @@ from cube_helper.cube_help import (load,
                                    add_categorical,
                                    aggregate_categorical,
                                    extract_categorical,
-                                   concatenate)
+                                   concatenate,
+                                   extract,
+                                   extract_boundries)
 from cube_helper.cube_loader import (load_from_dir,
                                      load_from_filelist,
                                      sort_by_earliest_date,
