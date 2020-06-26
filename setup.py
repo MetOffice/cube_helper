01766aa7ecc492463e1f9e860002417a4f6e7dfb
Jon Seddon <jon.seddon@metoffice.gov.uk>
2020-06-26 17:40:14 +0100

    PEP8

--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
 from distutils.core import setup
 setup(
-  name = 'cube_helper',
-  packages = ['cube_helper'],
-  version = '2.0.10',
+  name='cube_helper',
+  packages=['cube_helper'],
+  version='2.0.10',
   license='BSD 3-Clause License',
-  description = """Cube Helper is a package to make equalisation, 
+  description="""Cube Helper is a package to make equalisation,
   concatenation, and analysis of Iris cubes easier.
-  
-  It is written with reasearchers in mind and provides a good 
+
+  It is written with reasearchers in mind and provides a good
   degree of abstraction from many Iris functions and methods.""",
-  author = 'Met Office',
-  author_email = 'jon.seddon@metoffice.gov.uk',
-  url = 'https://github.com/MetOffice/cube_helper',
-  download_url = 'https://github.com/MetOffice/cube_helper/archive/v2.0.10.tar.gz',
-  keywords = ['climate', 'cmip', 'iris', 'cubes', 'preprocessing'],
+  author='Met Office',
+  author_email='jon.seddon@metoffice.gov.uk',
+  url='https://github.com/MetOffice/cube_helper',
+  download_url=('https://github.com/MetOffice/cube_helper/archive/'
+                'v2.0.10.tar.gz'),
+  keywords=['climate', 'cmip', 'iris', 'cubes', 'preprocessing'],
   install_requires=[
-      'scitools-iris'
+    'scitools-iris'
   ],
   classifiers=[
     'Development Status :: 3 - Alpha',
