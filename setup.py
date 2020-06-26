773a364d8e1b31a0b034e2d8617d5b85e8e1bc45
James Bedwell <https://theelectricflock@github.com>
2020-06-26 16:38:20 +0100

    fix version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'cube_helper',
   packages = ['cube_helper'],
-  version = '2.0.8',
+  version = '2.0.9',
   license='BSD 3-Clause License',
   description = """Cube Helper is a package to make equalisation, 
   concatenation, and analysis of iris cubes easier.
@@ -12,7 +12,7 @@ setup(
   author = 'Met Office',
   author_email = 'jon.seddon@metoffice.gov.uk',
   url = 'https://github.com/MetOffice/cube_helper',
-  download_url = 'https://github.com/MetOffice/cube_helper/archive/v2.0.7.tar.gz',
+  download_url = 'https://github.com/MetOffice/cube_helper/archive/v2.0.9.tar.gz',
   keywords = ['climate', 'cmip', 'iris', 'cubes', 'preprocessing'],
   classifiers=[
     'Development Status :: 3 - Alpha',
