295300bf7a92a3a002ed48bc3ab3334f97d090ef
Jon Seddon <jon.seddon@metoffice.gov.uk>
2020-10-08 12:28:34 +0100

    Module import cannot be at top of file in this case

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
 
-import cube_helper as ch
+import cube_helper as ch                        # noqa: E402
 
 # -- Project information -----------------------------------------------------
 
